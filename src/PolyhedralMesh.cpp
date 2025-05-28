135a742963a61b898755780fa0c20c3f0f2c1cd0
sofiacng <s259118@studenti.polito.it>
2025-05-28 18:20:42 +0200

    aggiornato fino a dodecaedro

--- src/PolyhedralMesh.cpp
+++ src/PolyhedralMesh.cpp
@@ -178,18 +178,18 @@ void Ottaedro(PolyhedralMesh& pm){
     }
 	
 	pm.Cell1DsExtrema.resize(pm.NumCell1Ds, 2);
-    pm.Cell1DsExtrema << 8, 10,
-                        8, 11,
-                        8, 12,
-                        8, 13,
-                        9, 10,
-                        9, 11,
-                        9, 12,
-                        9, 13,
-                        10, 12,
-                        10, 13,
-                        11, 12,
-                        11, 13;
+    pm.Cell1DsExtrema << 0, 2,
+                         0, 3,
+                         0, 4,
+                         0, 5,
+                         1, 2,
+                         1, 3,
+                         1, 4,
+                         1, 5,
+                         2, 4,
+                         2, 5,
+                         3, 4,
+                         3, 5;
 
     pm.NumCell2Ds = 8; // numero facce ottaedro
 	
@@ -200,26 +200,26 @@ void Ottaedro(PolyhedralMesh& pm){
 	pm.Cell2DsVertices.resize(pm.NumCell2Ds, 3);
 	
     pm.Cell2DsVertices <<
-        12, 10, 9,
-        12, 11, 9,
-        12, 11, 8,
-        12, 8, 10,
-        13, 8, 10,
-        13, 10, 9,
-        13, 9, 11,
-        13, 11, 8;
+							4, 2, 1,
+							4, 3, 1,
+							4, 3, 0,
+							4, 0, 2,
+							5, 0, 2,
+							5, 2, 1,
+							5, 1, 3,
+							5, 3, 0;
 		
 	pm.Cell2DsEdges.resize(pm.NumCell2Ds, 3);
 
     pm.Cell2DsEdges <<
-        26, 22, 24,
-        28, 23, 24,
-        28, 19, 20,
-        20, 18, 26,
-        21, 18, 27,
-        27, 22, 25,
-        25, 23, 29,
-        29, 19, 21;
+							8, 4, 6,
+							10, 5, 6,
+							10, 1, 2,
+							2, 0, 8,
+							3, 0, 9,
+							9, 4, 7,
+							7, 5, 11,
+							11, 1, 3;
 
 
     pm.Cell3DsId = 2;
@@ -231,19 +231,19 @@ void Ottaedro(PolyhedralMesh& pm){
     pm.Cell3DsFaces = pm.Cell2DsId;
 
 }
-/*
+
 
 void Dodecaedro(PolyhedralMesh& pm){
     pm.NumCell0Ds = 20; // numero vertici dodecaedro
 	
 	unsigned int i = 0;
-    for (i = 0; i < 8; i++){
+	pm.Cell0DsId.resize(pm.NumCell0Ds);
+    for (i = 0; i < 20; i++){
         pm.Cell0DsId[i] = i; // vettore degli id dei vertici
     }
-    for (i = 14; i < 26; i++){
-        pm.Cell0DsId[i-6] = i; // vettore degli id dei vertici
-    }
-
+	
+	pm.Cell0DsCoordinates.resize(pm.NumCell0Ds,3);
+	
     pm.Cell0DsCoordinates << 0,577350269, 0,577350269, 0,577350269,
                             0,577350269, -0,577350269, -0,577350269,
                             -0,577350269, 0,577350269, -0,577350269,
@@ -267,103 +267,94 @@ void Dodecaedro(PolyhedralMesh& pm){
 
 
     pm.NumCell1Ds = 30; //numero lati dodecaedro
-
-    for (i = 30; i < 60; i++){
-        pm.Cell1DsId[i-30] = i; // vettore degli id dei lati
+	
+	pm.Cell1DsId.resize(pm.NumCell1Ds);
+	
+    for (i = 0; i < 30; i++){
+        pm.Cell1DsId[i] = i; // vettore degli id dei lati
     }
+	
+	pm.Cell1DsExtrema.resize(pm.NumCell1Ds, 2);
 
-    pm.Cell1DsExtrema << 0, 22,
-                        0, 18,
-                        0, 14,
-                        6, 22,
-                        6, 19,
-                        6, 16,
-                        3, 23,
-                        3, 17,
-                        3, 19,
-                        5, 23,
-                        5, 15,
-                        5, 18,
-                        4, 14,
-                        4, 24,
-                        4, 20,
-                        2, 24,
-                        2, 16,
-                        2, 21,
-                        7, 17,
-                        7, 21,
-                        7, 25,
-                        1, 15,
-                        1, 18,
-                        1, 25,
-                        16, 17,
-                        14, 15,
-                        22, 24,
-                        23, 25,
-                        19, 18,
-                        21, 20;
+    pm.Cell1DsExtrema << 	0, 16,
+							0, 12,
+							0, 8,
+							6, 16,
+							6, 13,
+							6, 10,
+							3, 17,
+							3, 11,
+							3, 13,
+							5, 17,
+							5, 9,
+							5, 12,
+							4, 8,
+							4, 18,
+							4, 14,
+							2, 18,
+							2, 10,
+							2, 15,
+							7, 11,
+							7, 15,
+							7, 19,
+							1, 9,
+							1, 14,
+							1, 19,
+							16, 11,
+							14, 9,
+							22, 18,
+							23, 19,
+							19, 12,
+							21, 14;
 
     pm.NumCell2Ds = 12; // numero facce dodecaedro
 
-    for (i = 18; i < 30; i++){
-        pm.Cell2DsId[i-18] = i; // vettore degli id delle facce
+	pm.Cell2DsId.resize(pm.NumCell2Ds);
+	
+    for (i = 0; i < 12; i++){
+        pm.Cell2DsId[i] = i; // vettore degli id delle facce
     }
 
-    pm.Cell2DsVertices = 
-    {
-        {19, 6, 22, 0, 18},
-        {3, 19, 18, 5, 23},
-        {3, 17, 16, 6, 19},
-        {5, 18, 0, 14, 15},
-        {21, 7, 25, 1, 20},
-        {2, 21, 20, 4, 24},
-        {2, 16, 17, 7, 21},
-        {4, 20, 1, 15, 14},
-        {4, 14, 0, 22, 24},
-        {2, 24, 22, 6, 16},
-        {7, 17, 3, 23, 25},
-        {1, 25, 23, 5, 15}
-    };
+	pm.Cell2DsVertices.resize(pm.NumCell2Ds, 5);
+	
+    pm.Cell2DsVertices <<   13, 6, 16, 0, 12,
+							3, 13, 12, 5, 17,
+							3, 11, 10, 6, 13,
+							5, 12, 0, 8, 9,
+							15, 7, 19, 1, 14,
+							2, 15, 14, 4, 18,
+							2, 10, 11, 7, 15,
+							4, 14, 1, 9, 8,
+							4, 8, 0, 16, 18,
+							2, 18, 16, 6, 10,
+							7, 11, 3, 17, 19,
+							1, 19, 17, 5, 9;
+						
+	pm.Cell2DsEdges.resize(pm.NumCell2Ds, 5);
 
-    pm.Cell2DsEdges = 
-    {
-        {34, 33, 30, 31, 58},
-        {38, 58, 41, 39, 36},
-        {37, 54, 35, 34, 38},
-        {41, 31, 32, 55, 40},
-        {49, 50, 53, 52, 59},
-        {47, 59, 44, 43, 45},
-        {46, 54, 48, 49, 47},
-        {44, 52, 51, 55, 42},
-        {42, 32, 30, 56, 43},
-        {45, 56, 33, 35, 46},
-        {48, 37, 36, 57, 50},
-        {53, 57, 39, 40, 51}
-    };
-
-    pm.NumCell3Ds = 1;
+    pm.Cell2DsEdges << 		4, 3, 0, 1, 28,
+							8, 28, 11, 9, 6,
+							7, 24, 5, 4, 8,
+							11, 1, 2, 25, 10,
+							19, 20, 23, 22, 29,
+							17, 29, 14, 13, 15,
+							16, 24, 18, 19, 17,
+							14, 22, 21, 25, 12,
+							12, 2, 0, 26, 13,
+							15, 26, 3, 5, 16,
+							18, 7, 6, 27, 20,
+							23, 27, 9, 10, 21;
+    
 
     pm.Cell3DsId = 3; // id ottaedro
 
-    pm.Cell3DsVertices = 
-    {
-        {0, 1, 2, 3, 4, 5, 6, 7, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
-    };
-    //pm.Cell3DsVertices = pm.Cell0DsId
+    pm.Cell3DsVertices = pm.Cell0DsId;
 
-    pm.Cell3DsEdges = 
-    {
-        {30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59}
-    };
-    //pm.Cell3DsEdges = pm.Cell1DsId
+    pm.Cell3DsEdges = pm.Cell1DsId;
 
-    pm.Cell3DsFaces = 
-    {
-        {18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29}
-    };
-    //pm.Cell3DsFaces = pm.Cell2DsId
+    pm.Cell3DsFaces = pm.Cell2DsId;
 }
-*/
+
 void Icosaedro(PolyhedralMesh& pm){
 	pm.NumCell0Ds = 12; // numero vertici icosaedro
 	unsigned int i = 0;
